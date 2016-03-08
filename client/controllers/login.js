a965f953ac51aa5a4ce8d212cf7da758afa8a7b2
Christina Strynatka <cstrynatka@gmail.com>
2016-03-07 19:48:13 -0500

    Filled in information for signup JS page controller

--- client/controllers/login.js
+++ client/controllers/login.js
@@ -28,3 +28,17 @@ angular.module('Instagram')
 		};
 
 	});
+
+	google: {
+		url: '/auth/google',
+		authorizationEndpoint: 'https://accounts.google.com/o/oauth2/auth',
+		redirectUri: window.location.origin || window.location.protocol + '//' + window.location.host,
+		scope: ['profile', 'email'],
+		scopePrefix: 'openid',
+		scopeDelimiter: ' ',
+		requiredUrlParams: ['scope'],
+		optionalUrlParams: ['display'],
+		display: 'popup',
+		type: '2.0',
+		popupOptions: { width: 452, height: 633 }
+	}
