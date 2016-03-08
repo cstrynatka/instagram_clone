5702cca301a5a43efb1864cc7139a23ea3128aef
Christina Strynatka <cstrynatka@gmail.com>
2016-03-07 21:41:50 -0500

    Created new navbar block of code

--- controllers/navbar.js
+++ controllers/navbar.js
@@ -1,1 +1,11 @@
-navbar.js
+angular.module('Instagram')
+.controller('NavbarCtrl', function($scope, $rootScope, $window, $auth) {
+	$scope.isAuthenticated = function() {
+		return $auth.isAuthenticated();
+	};
+
+	$scope.logout = function() {
+		$auth.logout();
+		delete $window.localStorage.currentUser;
+	};
+});
