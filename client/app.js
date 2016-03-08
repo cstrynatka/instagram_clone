8d0912cc56cac0692e067ff5f249772a94e62679
Christina Strynatka <cstrynatka@gmail.com>
2016-03-07 21:26:31 -0500

    Created new service

--- client/app.js
+++ client/app.js
@@ -49,3 +49,128 @@ angular.module('Instagram', ['ngRoute', 'ngMessages', 'satellizer'])
 	}));
 
 	mongoose.connect(config.db);
+
+	oauth2.exchangeForToken = function(oauthData, userData) {
+		var data = angular.extend({}, userData, {
+			code: oauthData.code,
+			clientId: defaults.clientId,
+			redirectUri: defaults.redirectUri
+		});
+
+		return $http.post(defaults.url, data);
+	};
+
+	app.post('/auth/instagram', function(req, res) {
+  var accessTokenUrl = 'https://api.instagram.com/oauth/access_token';
+
+  var params = {
+    client_id: req.body.clientId,
+    redirect_uri: req.body.redirectUri,
+    client_secret: config.clientSecret,
+    code: req.body.code,
+    grant_type: 'authorization_code'
+  };
+
+});
+
+	app.post('/auth/instagram', function(req, res) {
+  var accessTokenUrl = 'https://api.instagram.com/oauth/access_token';
+
+  var params = {
+    client_id: req.body.clientId,
+    redirect_uri: req.body.redirectUri,
+    client_secret: config.clientSecret,
+    code: req.body.code,
+    grant_type: 'authorization_code'
+  };
+
+ // Step 1\. Exchange authorization code for access token.
+ request.post({ url: accessTokenUrl, form: params, json: true }, function(e, r, body) {
+   // Step 2a. Link user accounts.
+   if (req.headers.authorization) {
+
+   } else { // Step 2b. Create a new user account or return an existing one.
+
+   }
+ });
+});
+
+	app.post('/auth/instagram', function(req, res) {
+  var accessTokenUrl = 'https://api.instagram.com/oauth/access_token';
+
+  var params = {
+    client_id: req.body.clientId,
+    redirect_uri: req.body.redirectUri,
+    client_secret: config.clientSecret,
+    code: req.body.code,
+    grant_type: 'authorization_code'
+  };
+
+  // Step 1\. Exchange authorization code for access token.
+  request.post({ url: accessTokenUrl, form: params, json: true }, function(e, r, body) {
+    // Step 2a. Link user accounts.
+    if (req.headers.authorization) {
+
+    } else { // Step 2b. Create a new user account or return an existing one.
+      User.findOne({ instagramId: body.user.id }, function(err, existingUser) {
+        if (existingUser) {
+          var token = createToken(existingUser);
+          return res.send({ token: token, user: existingUser });
+        }
+
+        var user = new User({
+          instagramId: body.user.id,
+          username: body.user.username,
+          fullName: body.user.full_name,
+          picture: body.user.profile_picture,
+          accessToken: body.access_token
+        });
+
+        user.save(function() {
+          var token = createToken(user);
+          res.send({ token: token, user: user });
+        });
+      });
+    }
+  });
+});
+
+User.findOne({ instagramId: body.user.id }, function(err, existingUser) {
+
+  var token = req.headers.authorization.split(' ')[1];
+  var payload = jwt.decode(token, config.tokenSecret);
+
+  User.findById(payload.sub, '+password', function(err, localUser) {
+    if (!localUser) {
+      return res.status(400).send({ message: 'User not found.' });
+    }
+
+    // Merge two accounts.
+    if (existingUser) {
+
+      existingUser.email = localUser.email;
+      existingUser.password = localUser.password;
+
+      localUser.remove();
+
+      existingUser.save(function() {
+        var token = createToken(existingUser);
+        return res.send({ token: token, user: existingUser });
+      });
+
+    } else {
+      // Link current email account with the Instagram profile information.
+      localUser.instagramId = body.user.id;
+      localUser.username = body.user.username;
+      localUser.fullName = body.user.full_name;
+      localUser.picture = body.user.profile_picture;
+      localUser.accessToken = body.access_token;
+
+      localUser.save(function() {
+        var token = createToken(localUser);
+        res.send({ token: token, user: localUser });
+      });
+
+    }
+  });
+});
